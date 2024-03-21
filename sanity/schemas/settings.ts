3c05806bc291c64ec2b406e56d3cc6a1b60e50a0
papayahu <147315302+papayahu@users.noreply.github.com>
2024-03-21 17:06:38 +0800

    Update settings.ts
    Change display language

--- sanity/schemas/settings.ts
+++ sanity/schemas/settings.ts
@@ -4,30 +4,30 @@ import { FilterHorizontalIcon } from '~/assets'
 
 export default defineType({
   name: 'settings',
-  title: '网站设置',
+  title: '網站設置',
   type: 'document',
   icon: FilterHorizontalIcon,
   fields: [
     defineField({
       name: 'projects',
-      title: '项目展示列表',
-      description: '在 `/projects` 页面展示的项目列表',
+      title: '項目展示列表',
+      description: '在 `/projects` 頁面展示的項目列表',
       type: 'array',
       of: [{ type: 'reference', to: { type: 'project' } }],
     }),
 
     defineField({
       name: 'heroPhotos',
-      title: '首页图片',
-      description: '首页顶部的几张图片（推荐设置 6 张）',
+      title: '首頁圖片',
+      description: '首頁頂部的幾張圖片（推薦設置 6 張）',
       type: 'array',
       of: [{ type: 'image' }],
     }),
 
     defineField({
       name: 'resume',
-      title: '简历',
-      description: '在主页侧边栏展示的简历信息（留空就不展示）',
+      title: '簡歷',
+      description: '在主頁側邊欄展示的簡歷訊息（留空就不展示）',
       type: 'array',
       of: [
         {
@@ -35,20 +35,20 @@ export default defineType({
           fields: [
             defineField({
               name: 'company',
-              title: '公司名称',
+              title: '公司名稱',
               type: 'string',
               validation: (Rule) => Rule.required(),
             }),
             defineField({
               name: 'title',
-              title: '职位',
+              title: '職位',
               type: 'string',
               validation: (Rule) => Rule.required(),
             }),
             defineField({
               name: 'logo',
               title: '公司 Logo',
-              description: '建议尺寸 100x100px 正方形裁切',
+              description: '建議尺寸 100x100px 正方形裁切',
               type: 'image',
               options: {
                 hotspot: true,
@@ -57,13 +57,13 @@ export default defineType({
             }),
             defineField({
               name: 'start',
-              title: '开始时间',
+              title: '開始時間',
               type: 'string',
               validation: (Rule) => Rule.required(),
             }),
             defineField({
               name: 'end',
-              title: '结束时间（留空会显示“至今”）',
+              title: '結束時間（留空會顯示“至今”）',
               type: 'string',
             }),
           ],
@@ -89,7 +89,7 @@ export default defineType({
   preview: {
     select: {},
     prepare: () => ({
-      title: '网站设置',
+      title: '網站設置',
     }),
   },
 })
