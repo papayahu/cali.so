b76612a64ae4907169e7c5c00c8c597827339378
papayahu <147315302+papayahu@users.noreply.github.com>
2024-04-26 22:22:43 +0800

    Update page.tsx

--- app/(main)/memos/page.tsx
+++ app/(main)/memos/page.tsx
@@ -8,16 +8,16 @@ import { Container } from '~/components/ui/Container'
 //import ThankYouLetterScreenshot1 from './Arc aagD26w9@2x.png'
 //import ThankYouLetterScreenshot2 from './Arc ynleUdHy@2x.png'
 
-const title = '我的Mastodon隨記園地'
-const description =
-  '一些生活碎片'
-
 const myTimeline = new MastodonTimeline.Init({
 instanceUrl: "https://mastodon.social",
 timelineType: "profile",
 userId: "112336750066396391",
 profileName: "@papayahu_",
 });
+
+const title = '我的Mastodon隨記園地'
+const description =
+  '一些生活碎片'
 
 export const metadata = {
   title,
@@ -33,6 +33,18 @@ export const metadata = {
   },
 }
 
+export default function AskMeAnythingPage() {
+  return (
+    <Container className="mt-16 sm:mt-24">
+      <header className="max-w-2xl">
+        <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+          Ask Me Anything / 一对一咨询
+        </h1>
+        <p className="my-6 text-base text-zinc-600 dark:text-zinc-400">
+          <Balancer>{description}</Balancer>
+        </p>
+      </header>
+  
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@idotj/mastodon-embed-timeline@4.4.2/dist/mastodon-timeline.min.css" integrity="sha256-1UGgxsonaMCfOEnVOL89aMKSo3GEAmaRP0ISbsWa6lU=" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/@idotj/mastodon-embed-timeline@4.4.2/dist/mastodon-timeline.umd.js" integrity="sha256-E6WPG6iq+qQIzvu3HPJJxoAeRdum5siq13x4ITjyxu8=" crossorigin="anonymous"></script>
 
@@ -41,3 +53,6 @@ export const metadata = {
     <div class="mt-loading-spinner"></div>
   </div>
 </div>
+
+  )
+}
