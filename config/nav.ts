97882412625674250c47fb24d944d1b6754a7eaa
papayahu <147315302+papayahu@users.noreply.github.com>
2024-04-26 21:41:58 +0800

    Update nav.ts

--- config/nav.ts
+++ config/nav.ts
@@ -1,8 +1,9 @@
 export const navigationItems = [
-  { href: '/', text: '首页' },
-  { href: '/blog', text: '博客' },
-  { href: '/projects', text: '项目' },
-  { href: '/guestbook', text: '留言墙' },
-  { href: '/ama', text: 'AMA 咨询' },
+  { href: '/', text: '首頁' },
+  { href: '/blog', text: '部落格' },
+  { href: '/projects', text: '項目' },
+  { href: '/memos', text: '隨記' },
+  { href: '/guestbook', text: '留言牆' },
+ // { href: '/ama', text: 'AMA 咨询' },
   // { href: '/about', text: '关于' },
 ]
