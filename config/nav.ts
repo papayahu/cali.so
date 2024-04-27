3d2d7d618925555ef386fd218c3ff90e424e0913
papayahu <147315302+papayahu@users.noreply.github.com>
2024-04-27 14:50:32 +0800

    更新 nav.ts

--- config/nav.ts
+++ config/nav.ts
@@ -2,7 +2,7 @@ export const navigationItems = [
   { href: '/', text: '首頁' },
   { href: '/blog', text: '部落格' },
   { href: '/projects', text: '項目' },
-  { href: '/memos', text: '隨記' },
+ // { href: '/memos', text: '隨記' },
   { href: '/guestbook', text: '留言牆' },
  // { href: '/ama', text: 'AMA 咨询' },
   // { href: '/about', text: '关于' },
