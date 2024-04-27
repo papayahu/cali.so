0283a24b3d22578266755f6e46bab1a5ce55ce1a
papayahu <147315302+papayahu@users.noreply.github.com>
2024-04-27 17:17:24 +0800

    Update page.tsx

--- app/(main)/memos/page.tsx
+++ app/(main)/memos/page.tsx
@@ -1,58 +1,32 @@
-import Image from 'next/image'
-import Balancer from 'react-wrap-balancer'
+<!DOCTYPE html>
 
-import { RichLink } from '~/components/links/RichLink'
-import { Container } from '~/components/ui/Container'
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Your site title</title>
+    <!-- CSS -->
+    <link href="app/(main)/memos/mastodon-timeline.min.css" rel="stylesheet" />
+  </head>
 
-//import AlipayQR from './alipay-qr.jpg'
-//import ThankYouLetterScreenshot1 from './Arc aagD26w9@2x.png'
-//import ThankYouLetterScreenshot2 from './Arc ynleUdHy@2x.png'
+  <body>
+        <!-- Mastodon Timeline -->
+        <div id="mt-container" class="mt-container">
+          <div class="mt-body" role="feed">
+            <div class="mt-loading-spinner"></div>
+          </div>
+        </div>  
 
-const myTimeline = new MastodonTimeline.Init({
-instanceUrl: "https://mastodon.social",
-timelineType: "profile",
-userId: "112336750066396391",
-profileName: "@papayahu_",
-});
+    <!-- JavaScript -->
+    <script src="app/(main)/memos/mastodon-timeline.umd.js"></script>
+    <script>
+      const myTimeline = new MastodonTimeline.Init({
+        instanceUrl: "https://mastodon.social",
+        timelineType: "profile",
+        userId: "112336750066396391",
+        profileName: "@papayahu_",
+      });    
 
-const title = '我的Mastodon隨記園地'
-const description =
-  '一些生活碎片'
-
-export const metadata = {
-  title,
-  description,
-  openGraph: {
-    title,
-    description,
-  },
-  twitter: {
-    title,
-    description,
-    card: 'summary_large_image',
-  },
-}
-
-export default function AskMeAnythingPage() {
-  return (
-    <Container className="mt-16 sm:mt-24">
-      <header className="max-w-2xl">
-        <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-          Ask Me Anything / 一对一咨询
-        </h1>
-        <p className="my-6 text-base text-zinc-600 dark:text-zinc-400">
-          <Balancer>{description}</Balancer>
-        </p>
-      </header>
-  
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@idotj/mastodon-embed-timeline@4.4.2/dist/mastodon-timeline.min.css" integrity="sha256-1UGgxsonaMCfOEnVOL89aMKSo3GEAmaRP0ISbsWa6lU=" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/@idotj/mastodon-embed-timeline@4.4.2/dist/mastodon-timeline.umd.js" integrity="sha256-E6WPG6iq+qQIzvu3HPJJxoAeRdum5siq13x4ITjyxu8=" crossorigin="anonymous"></script>
-
-  <div id="mt-container" class="mt-container">
-  <div class="mt-body" role="feed">
-    <div class="mt-loading-spinner"></div>
-  </div>
-</div>
-
-  )
-}
+    </script>
+  </body>
+</html>
