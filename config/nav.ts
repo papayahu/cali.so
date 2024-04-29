4b4473226d4ebcf2a3766968733699794f3775d1
papayahu <147315302+papayahu@users.noreply.github.com>
2024-04-29 19:04:26 +0800

    Update nav.ts

--- config/nav.ts
+++ config/nav.ts
@@ -3,6 +3,7 @@ export const navigationItems = [
   { href: '/blog', text: '部落格' },
   { href: '/projects', text: '項目' },
   { href: 'https://papayahu.github.io/memos/', text: '隨記' },
+  { href: '/music', text: '樂廊' },  
   { href: '/guestbook', text: '留言牆' },
  // { href: '/ama', text: 'AMA 咨询' },
   // { href: '/about', text: '关于' },
