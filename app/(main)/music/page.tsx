6ac5552869ace2729cc3bbf72f5b8bb021c5f2f5
papayahu <147315302+papayahu@users.noreply.github.com>
2024-04-29 19:11:10 +0800

    Update page.tsx

--- app/(main)/music/page.tsx
+++ app/(main)/music/page.tsx
@@ -1,16 +1,10 @@
-import Image from 'next/image'
-import Balancer from 'react-wrap-balancer'
+import Image from 'next/image';
+import Balancer from 'react-wrap-balancer';
+import { RichLink } from '~/components/links/RichLink';
+import { Container } from '~/components/ui/Container';
 
-import { RichLink } from '~/components/links/RichLink'
-import { Container } from '~/components/ui/Container'
-
-//import AlipayQR from './alipay-qr.jpg'
-//import ThankYouLetterScreenshot1 from './Arc aagD26w9@2x.png'
-//import ThankYouLetterScreenshot2 from './Arc ynleUdHy@2x.png'
-
-const title = '音樂光廊'
-const description =
-  '網易雲自用歌單分享'
+const title = '音樂光廊';
+const description = '網易雲自用歌單分享';
 
 export const metadata = {
   title,
@@ -24,7 +18,7 @@ export const metadata = {
     description,
     card: 'summary_large_image',
   },
-}
+};
 
 export default function MusicLightGallery() {
   return (
@@ -43,62 +37,31 @@ export default function MusicLightGallery() {
         <p>歡迎一起來聽歌！</p>
         <ul>
           <li>
-            <b>beacon</b>
-            ：
-          <RichLink href="https://heo-music-xi.vercel.app" target="_blank">
-              揮別沮喪的網，迎接希望的光
-            </RichLink>
+            <b>beacon</b>：<RichLink href="https://heo-music-xi.vercel.app" target="_blank">揮別沮喪的網，迎接希望的光</RichLink>
           </li>
           <li>
-            <b>morning</b>
-            ：
-          <RichLink href="https://papayahu.github.io/morning" target="_blank">
-              適合早晨睡眼惺忪時聽的歌曲
-            </RichLink>
+            <b>morning</b>：<RichLink href="https://papayahu.github.io/morning" target="_blank">適合早晨睡眼惺忪時聽的歌曲</RichLink>
           </li>
           <li>
-            <b>faves</b>
-            ：
-          <RichLink href="https://papayahu.github.io/faves" target="_blank">
-              精選
-            </RichLink>
+            <b>faves</b>：<RichLink href="https://papayahu.github.io/faves" target="_blank">精選</RichLink>
           </li>
           <li>
-            <b>listening</b>
-            ：
-          <RichLink href="https://papayahu.github.io/listening" target="_blank">
-              純音精選
-            </RichLink>
+            <b>listening</b>：<RichLink href="https://papayahu.github.io/listening" target="_blank">純音精選</RichLink>
           </li>          
           <li>
-            <b>2021</b>
-            ：
-          <RichLink href="https://papayahu.github.io/2021" target="_blank">
-              2021年度歌單
-            </RichLink>
+            <b>2021</b>：<RichLink href="https://papayahu.github.io/2021" target="_blank">2021年度歌單</RichLink>
           </li> 
           <li>
-            <b>2022</b>
-            ：
-          <RichLink href="https://papayahu.github.io/2022" target="_blank">
-              2022年度歌單
-            </RichLink>
+            <b>2022</b>：<RichLink href="https://papayahu.github.io/2022" target="_blank">2022年度歌單</RichLink>
           </li> 
           <li>
-            <b>2023</b>
-            ：
-          <RichLink href="https://papayahu.github.io/2023" target="_blank">
-              2023年度歌單
-            </RichLink>
+            <b>2023</b>：<RichLink href="https://papayahu.github.io/2023" target="_blank">2023年度歌單</RichLink>
           </li> 
           <li>
-            <b>heart</b>
-            ：
-          <RichLink href="https://papayahu.github.io/heart" target="_blank">
-              最新1000首紅心歌曲
-            </RichLink>
+            <b>heart</b>：<RichLink href="https://papayahu.github.io/heart" target="_blank">最新1000首紅心歌曲</RichLink>
           </li>     
+        </ul>
       </article>
     </Container>
-  )
+  );
 }
