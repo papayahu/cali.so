184e3fa4485e1fbab590626103a47e72f13f6182
papayahu <147315302+papayahu@users.noreply.github.com>
2024-04-30 18:51:04 +0800

    🔥 customize the personal information

--- app/(main)/guestbook/page.tsx
+++ app/(main)/guestbook/page.tsx
@@ -30,7 +30,7 @@ export default async function GuestBookPage() {
     <Container className="mt-16 sm:mt-32">
       <header className="max-w-2xl">
         <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-          歡迎來到我的塗鴉牆 | Graffiti Wall
+          歡迎來到我的塗鴉牆
         </h1>
         <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
           <Balancer>{description}</Balancer>
