b97b29f4ba8624e3405f8dbf738df4ba87611fb3
papayahu <147315302+papayahu@users.noreply.github.com>
2024-05-06 19:23:56 +0800

    🔥 customize the personal information

--- lib/seo.ts
+++ lib/seo.ts
@@ -1,10 +1,10 @@
 export const seo = {
   title: 'Tracy Hu | 探險家、養生黨、嘻哈迷、筆耕者',
   description:
-    '我叫 Cali，一名开发者，设计师，细节控，同时也是佐玩创始人，目前带领着佐玩致力于创造一个充满创造力的工作环境，同时鼓励团队创造影响世界的产品。',
+    '我是 Tracy，INFP，尋寶塢管理者。我熱愛閱讀、音樂、Bujo、探索新鮮的事物，喜歡帶著好奇心在知識的海洋裡悠遊。',
   url: new URL(
     process.env.NODE_ENV === 'production'
-      ? 'https://cali.so'
+      ? 'https://papayahu-so.vercel.app'
       : 'http://localhost:3000'
   ),
 } as const
