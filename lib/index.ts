33096a89bc1aaeac1a77598b54daad8be48f5755
papayahu <147315302+papayahu@users.noreply.github.com>
2024-05-07 12:59:30 +0800

    🔥 customize the personal information

--- lib/index.ts
+++ lib/index.ts
@@ -3,7 +3,8 @@ import { env } from '~/env.mjs'
 export function url(path = '') {
   const baseUrl =
     process.env.NODE_ENV === 'production'
-      ? env.NEXT_PUBLIC_SITE_URL
+     // ? env.NEXT_PUBLIC_SITE_URL
+      ? 'https://papayahu-so.vercel.app'
       : 'http://localhost:3000'
 
   return new URL(path, baseUrl)
