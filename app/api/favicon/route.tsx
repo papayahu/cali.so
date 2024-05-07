88530986298b8bbdafa5033747456546a67b7a5a
papayahu <147315302+papayahu@users.noreply.github.com>
2024-05-07 17:07:34 +0800

    Update route.tsx

--- app/api/favicon/route.tsx
+++ app/api/favicon/route.tsx
@@ -13,13 +13,13 @@ function getKey(url: string) {
 
 const faviconMapper: { [key: string]: string } = {
   '((?:zolplay.cn)|(?:zolplay.com)|(?:cn.zolplay.com))':
-    'https://cali.so/favicons/zolplay.png',
-  '(?:github.com)': 'https://cali.so/favicons/github.png',
+    'https://raw.githubusercontent.com/papayahu/papayahu.so/main/public/favicons/zolplay.png',
+  '(?:github.com)': 'https://raw.githubusercontent.com/papayahu/papayahu.so/main/public/favicons/github.png',
   '((?:t.co)|(?:twitter.com)|(?:x.com))':
-    'https://cali.so/favicons/twitter.png',
-  'coolshell.cn': 'https://cali.so/favicons/coolshell.png',
-  'vercel.com': 'https://cali.so/favicons/vercel.png',
-  'nextjs.org': 'https://cali.so/favicons/nextjs.png',
+    'https://raw.githubusercontent.com/papayahu/papayahu.so/main/public/favicons/twitter.png',
+  'coolshell.cn': 'https://raw.githubusercontent.com/papayahu/papayahu.so/main/public/favicons/coolshell.png',
+  'vercel.com': 'https://raw.githubusercontent.com/papayahu/papayahu.so/main/public/favicons/vercel.png',
+  'nextjs.org': 'https://raw.githubusercontent.com/papayahu/papayahu.so/main/public/favicons/nextjs.png',
 }
 
 function getPredefinedIconForUrl(url: string): string | undefined {
@@ -63,7 +63,7 @@ export async function GET(req: NextRequest) {
     return NextResponse.error()
   }
 
-  let iconUrl = 'https://cali.so/favicon_blank.png'
+  let iconUrl = 'https://raw.githubusercontent.com/papayahu/papayahu.so/main/public/favicon_blank.png'
 
   try {
     const predefinedIcon = getPredefinedIconForUrl(url)
