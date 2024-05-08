49b4e6a91741706cbe05b89aeaa518caf16d34b2
papayahu <147315302+papayahu@users.noreply.github.com>
2024-05-08 08:33:18 +0800

    Updated seo.ts

--- lib/seo.ts
+++ lib/seo.ts
@@ -1,5 +1,5 @@
 export const seo = {
-  title: 'Tracy Hu | 探險家、養生黨、嘻哈迷、筆耕者',
+  title: 'Tracy Hu | 探險家、養生黨、音樂迷、筆耕者',
   description:
     '我是 Tracy，INFP，尋寶塢管理者。我熱愛閱讀、音樂、Bujo、探索新鮮的事物，喜歡帶著好奇心在知識的海洋裡悠遊。',
   url: new URL(
