6bf86f8e640121287e6233b6c4139910f5354a23
papayahu <147315302+papayahu@users.noreply.github.com>
2024-07-14 14:29:15 +0800

    更新 page.tsx

--- app/(main)/projects/page.tsx
+++ app/(main)/projects/page.tsx
@@ -28,7 +28,7 @@ export default function ProjectsPage() {
           舊閣樓裡閃閃發光的東西。
         </h1>
         <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-          這裡存放著一些珍藏的小物，可能是新嘗試、作品或別的有趣的內容。
+          這裡存放著一些珍藏的小物，可能是新嘗試、作品或別的有趣的內容。（整理資料ing，持續更新中...）
         </p>
       </header>
       <div className="mt-16 sm:mt-20">
